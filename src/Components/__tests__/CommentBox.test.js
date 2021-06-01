0c0a3551914752b9d7de0b97c0f209ecd154732c
marcus-baumgardner <dwayne.jetski@gmail.com>
2021-06-01 17:10:16 -0500

    moved the code in before each that isn't relevant to ALL tests into a before each within a describe block for the tests it is relevant to.

--- src/Components/__tests__/CommentBox.test.js
+++ src/Components/__tests__/CommentBox.test.js
@@ -10,13 +10,6 @@ let form;
 
 beforeEach(() => {
     wrapped = mount(<CommentBox />);
-    textArea = wrapped.find('textarea');
-    form = wrapped.find('form');
-
-    newComment = textArea.simulate('change', {
-        target: { value: 'new comment', name: 'userInput'}
-    });
-    
 });
 
 afterEach(() => {
@@ -24,28 +17,33 @@ afterEach(() => {
 });
 
 it('has a text area and a button', () => {
-
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a text area that users can type in', () => {
-    wrapped.find('textarea').simulate('change', {
-        target: { value: 'new comment', name: 'userInput' }
+describe('the text area', () => {
+    beforeEach(() => {
+        textArea = wrapped.find('textarea');
+        form = wrapped.find('form');
+
+        newComment = textArea.simulate('change', {
+            target: { value: 'new comment', name: 'userInput'}
+        });
     });
-    wrapped.update();
-    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-});
 
-it('empties the textarea on form submission', () => {
-    newComment;
-
-    wrapped.update();
-    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-
-    form.simulate('submit');
-    wrapped.update();
+    it('has a text area that users can type in', () => {
+        newComment;
+        wrapped.update();
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+    });
     
-    expect(wrapped.find('textarea').prop('value')).toEqual('');
-})
-
+    it('empties the textarea on form submission', () => {
+        newComment;
+        wrapped.update();
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+        
+        form.simulate('submit');
+        wrapped.update();   
+        expect(wrapped.find('textarea').prop('value')).toEqual('');
+    });
+});
